6fd8eb654cb96cbb9bfd80beb80a8c0cce166209
e.alemasov <alemasov.evgeniy@mail.ru>
2022-08-13 14:14:52 +0300

    Добавил отправку вопроса в группу организаторов.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import telebot
 # Создаем экземпляр бота
-bot = telebot.TeleBot('5508554128:AAF3QQ3eHfjm4RiZgWE6xbiU9JWXG0Klons')
+bot = telebot.TeleBot('***')
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+    bot.send_message(m.chat.id, 'Приветствуем! '
+                                '\nЭтот бот создан для того, чтобы заранее задать вопрос по Писанию, жизни церкви, хождению перед Богом. '
+                                '\nБратья-служители постараются ответить на этот вопрос на ближайших библейских курсах для членов церкви.')
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+    bot.send_message(message.chat.id, 'Ваш вопрос: "' + message.text + '" принят в разработку. '
+                                                                       '\nЖелаем вам Божьих благословений!')
+    bot.send_message(0, 'Поступил анонимный вопрос: \n"'+ message.text+'"')
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
